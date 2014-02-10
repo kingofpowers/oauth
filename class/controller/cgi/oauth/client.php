f4b28e25140dd0a5322748ef0c1d877cd79074c0
Jia Huang <jia.huang@geneegroup.com>
2014-02-10 20:04:12 +0800

    fix bug

--- class/controller/cgi/oauth/client.php
+++ class/controller/cgi/oauth/client.php
@@ -1,38 +1,36 @@
 <?php
 
-namespace Controller\CGI\OAuth {
+namespace Controller\CGI\OAuth;
 
-	class Client extends \Controller\CGI {
+class Client extends \Controller\CGI {
 
-		private $_client;
+	private $_client;
 
-		function action_auth() {
+	function actionAuth() {
 
-			$form = $this->form();
+		$form = $this->form();
 
-			$source = $form['source'];
-            $client = \Gini\IoC::construct('\Gini\OAuth\Client', $source);
-            
-			if (isset($form['error'])) {
-				$_SESSION['oauth.client.token'][$source] = '@'.$form['error'];
-			}
-			elseif (isset($form['code'])) {
-				// got authorization code, try to acquire access token
-                $client->fetchAccessToken('authorization_code', ['code'=>$form['code']]);
-			}
-			else {
-				// start oauth process...
-				$_SESSION['oauth.client.redirect_uri'][$source] = $form['redirect_uri'] ?: '/';
-                $client->authorize();
-                return;
-			}
-
-			// redirect to original place
-	    	$redirect_uri = $_SESSION['oauth.client.redirect_uri'][$source];
-	    	unset($_SESSION['oauth.client.redirect_uri'][$source]);
-	    	$this->redirect($redirect_uri);
+		$source = $form['source'];
+        $client = \Gini\IoC::construct('\Gini\OAuth\Client', $source);
+        
+		if (isset($form['error'])) {
+			$_SESSION['oauth.client.token'][$source] = '@'.$form['error'];
+		}
+		elseif (isset($form['code'])) {
+			// got authorization code, try to acquire access token
+            $client->fetchAccessToken('authorization_code', ['code'=>$form['code']]);
+		}
+		else {
+			// start oauth process...
+			$_SESSION['oauth.client.redirect_uri'][$source] = $form['redirect_uri'] ?: '/';
+            $client->authorize();
+            return;
 		}
 
+		// redirect to original place
+    	$redirect_uri = $_SESSION['oauth.client.redirect_uri'][$source];
+    	unset($_SESSION['oauth.client.redirect_uri'][$source]);
+    	$this->redirect($redirect_uri);
 	}
 
 }
--- class/controller/cgi/oauth/server.php
+++ class/controller/cgi/oauth/server.php
@@ -2,61 +2,59 @@
 
 // It's just a sample server controller, please override it.
 
-namespace Controller\CGI\OAuth {
+namespace Controller\CGI\OAuth;
 
-    class Server extends \Controller\CGI {
+class Server extends \Controller\CGI {
 
-        function action_auth() {
+    function actionAuth() {
 
-            $form = $this->form();
-            
-            $server = \Gini\IoC::construct('\Gini\OAuth\Authorization');
-            
-            if (!$server->isValid()) return false;
-
-            // check if user is logged in?
-            if (!\Gini\Auth::isLoggedIn()) {
-                $_SESSION['#LOGIN_REFERER'] = URL('oauth/server/auth', $this->form('get'));
-                $this->redirect(\Gini\Config::get('oauth.server')['login_url']);
-            }
-
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                if ($form['authorize']) {
-                    // Generate an authorization code
-                    $url = $server->authorize(\Gini\Auth::userName());
-                }
-                else {
-                    $url = $server->deny();
-                }
-                $this->redirect($url); 
-            }
-
-            return \Gini\IoC::construct('\Gini\CGI\Response\HTML', V('phtml/oauth/authorize', [
-                'form' => $form,
-                'client' => $server->clientDetails()
-            ]));
-        }
-
-        function action_token() {
-            $server = \Gini\IoC::construct('\Gini\OAuth\Authorization');
-            $response = $server->issueAccessToken();
-            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', $response);
-        }
-
-        function action_user() {
-            $resource = \Gini\IoC::construct('\Gini\OAuth\Resource', $_GET['access_token']);
-            if ($resource->isValid()) {
-                $username = $resource->getUserName();
-                $user = a('user', ['username'=>$username]);
-                return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
-                     'username' => $username,
-                     'name' => $user->name,
-                     'email' => $user->email,
-                ]);
-            }
-            return false;
-        }
+        $form = $this->form();
         
+        $server = \Gini\IoC::construct('\Gini\OAuth\Authorization');
+        
+        if (!$server->isValid()) return false;
+
+        // check if user is logged in?
+        if (!\Gini\Auth::isLoggedIn()) {
+            $_SESSION['#LOGIN_REFERER'] = URL('oauth/server/auth', $this->form('get'));
+            $this->redirect(\Gini\Config::get('oauth.server')['login_url']);
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if ($form['authorize']) {
+                // Generate an authorization code
+                $url = $server->authorize(\Gini\Auth::userName());
+            }
+            else {
+                $url = $server->deny();
+            }
+            $this->redirect($url); 
+        }
+
+        return \Gini\IoC::construct('\Gini\CGI\Response\HTML', V('phtml/oauth/authorize', [
+            'form' => $form,
+            'client' => $server->clientDetails()
+        ]));
     }
 
+    function actionToken() {
+        $server = \Gini\IoC::construct('\Gini\OAuth\Authorization');
+        $response = $server->issueAccessToken();
+        return \Gini\IoC::construct('\Gini\CGI\Response\JSON', $response);
+    }
+
+    function actionUser() {
+        $resource = \Gini\IoC::construct('\Gini\OAuth\Resource', $_GET['access_token']);
+        if ($resource->isValid()) {
+            $username = $resource->getUserName();
+            $user = a('user', ['username'=>$username]);
+            return \Gini\IoC::construct('\Gini\CGI\Response\JSON', [
+                 'username' => $username,
+                 'name' => $user->name,
+                 'email' => $user->email,
+            ]);
+        }
+        return false;
+    }
+    
 }
